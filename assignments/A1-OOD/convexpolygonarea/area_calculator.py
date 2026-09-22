16a3758a38b5acf01e62133f25e9d83f95b615db
Danell <150396721+eggnox@users.noreply.github.com>
2026-09-22 03:48:46 +0000

    Part1.2

--- assignments/A1-OOD/convexpolygonarea/area_calculator.py
+++ assignments/A1-OOD/convexpolygonarea/area_calculator.py
@@ -20,4 +20,32 @@ class PolygonAreaCalculator:
 
         return instance
 
+        @classmethod
+    def get_instance(cls) -> "PolygonAreaCalculator":
+        """Return the singleton calculator instance."""
+        return cls()
+
+    @staticmethod
+    def shoelace_term(first: Point, second: Point) -> int:
+        """Calculate one term of the shoelace formula."""
+        return first[0] * second[1] - first[1] * second[0]
+
+    def calculate_area(self, polygon: Polygon) -> float:
+        """Calculate and return the area of a polygon."""
+        vertex_total: int = len(polygon)
+
+        if vertex_total < 3:
+            return 0.0
+
+        area_sum: int = 0
+        index: int
+
+        for index in range(vertex_total):
+            first: Point = polygon.get_vertex(index)
+            second: Point = polygon.get_vertex(
+                (index + 1) % vertex_total
+            )
+            area_sum += self.shoelace_term(first, second)
+
+        return abs(area_sum) / 2.0
     
